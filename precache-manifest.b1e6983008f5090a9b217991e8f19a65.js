ba942596251563df035d9d50b081bae50c4e0053
Bojana Drangova <bojana.drangova@datajob.se>
2019-10-09 17:03:12 +0200

    Updates

---
+++ precache-manifest.b1e6983008f5090a9b217991e8f19a65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33af0bedc164fb438997fd3703bf7b8d",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "c0a399104b1508191add",
+    "url": "/rolodex/static/css/main.7c39f2f4.chunk.css"
+  },
+  {
+    "revision": "102a9b24fd9babe5b595",
+    "url": "/rolodex/static/js/2.a393edc3.chunk.js"
+  },
+  {
+    "revision": "c0a399104b1508191add",
+    "url": "/rolodex/static/js/main.3ad26de8.chunk.js"
+  },
+  {
+    "revision": "c881b687bd97746f9202",
+    "url": "/rolodex/static/js/runtime-main.a420b8c5.js"
+  }
+]);
